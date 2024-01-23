cb3eba763100c720b1391b4dad8c6e5488874c27
Aira Arima <arimacoelhoaira@gmail.com>
2024-01-23 17:48:48 -0300

    chore: initial structuring backend

---
+++ backend-orange/src/main/java/com/hackathon/backendorange/BackendOrangeApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.backendorange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendOrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendOrangeApplication.class, args);
+	}
+
+}
---
+++ backend-orange/src/test/java/com/hackathon/backendorange/BackendOrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.backendorange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendOrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
