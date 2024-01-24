a35dc720a1573f8c50f00c1cf486b292266759fb
Gil Rudge <gilblas07@hotmail.com>
2024-01-24 11:05:54 -0300

    criada paleta de cores no objeto de configuração do tema e configuração do formulário para envio de dados

---
+++ frontend-orange/src/utils/Theme.jsx
@@ -0,0 +1,29 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+export const theme = createTheme({
+
+  palette:{
+    primary:{
+      main: '#111133',
+      light:'#444466',
+      dark:'#222244',
+      contrastText: '#EDEFF2'      
+    },
+    secondary:{
+      main: '#FF5522',
+      secondaryMain:'#FF8833',
+      light:'#FFAA66',
+      secondaryLight:'#FFCC99',
+      dark:'#CC4400',
+      secondaryDark:'#993300',
+      darkest:'#662200',
+      contrastText: '#FFEECC',
+      secondContrastText: '#FFCC99'      
+    },
+    neutral:{
+      
+    }
+  }
+
+})
