12626597e3614ed331acf9e8f7ee5a6885d9dd8b
Matheus rabelo <matheusrabelo520@gmail.com>
2024-01-24 22:51:16 -0300

    feat: Criando o saveUser no controller

--- backend-orange/src/main/java/com/hackathon/backendorange/controller/UserController.java
+++ backend-orange/src/main/java/com/hackathon/backendorange/controller/UserController.java
@@ -1,11 +1,41 @@
 package com.hackathon.backendorange.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.hackathon.backendorange.controller.assembler.UserAssembler;
+import com.hackathon.backendorange.dto.UserDTO;
+import com.hackathon.backendorange.model.User;
+import com.hackathon.backendorange.service.UserService;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+	
+	@Autowired
+	private UserAssembler userAssembler;
 
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("/create")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<UserDTO> registrarUsuario(@RequestBody UserDTO userDTO) {
+		try {
+			User user = userAssembler.toEntity(userDTO);
+			@SuppressWarnings("unused")
+			UserDTO convertedDTO = userAssembler.toDTO(user);
+			UserDTO savedUserDTO = userService.saveUser(userDTO);
+			return new ResponseEntity<>(savedUserDTO, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
 	
 }
