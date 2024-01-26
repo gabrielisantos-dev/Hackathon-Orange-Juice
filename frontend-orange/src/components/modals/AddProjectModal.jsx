db8b52468b6c3bb3992f5cb46dd1aa3676db564e
gabrielicristina <gabrielicristina189@gmail.com>
2024-01-26 16:47:26 -0300

    Initial commit for the main-card branch

---
+++ frontend-orange/src/components/modals/AddProjectModal.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Modal, Button, TextField, Typography, Box, Grid } from '@mui/material';
+
+const AddProjectModal = ({ open, close }) => {
+    const [projectData, setProjectData] = useState({
+        title: '',
+        tags: '',
+        link: '',
+        description: '',
+    });
+
+    return();
+};
