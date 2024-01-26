ac3370de146a73b915b509f55272821673f8f0e5
Gil Rudge <gilblas07@hotmail.com>
2024-01-25 21:02:04 -0300

    responsividade do main-header para mobile

--- frontend-orange/src/components/main-header/Header.jsx
+++ frontend-orange/src/components/main-header/Header.jsx
@@ -2,11 +2,16 @@ import { Box, ThemeProvider, Typography } from "@mui/material";
 import {theme} from '../../utils/Theme';
 import logoOrange from '../../assets/logo-orange/logo-orange.svg';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import profilePicture from '../../assets/profile-picture/picture.svg'
+import profilePicture from '../../assets/profile-picture/picture.svg';
+import MenuIcon from '@mui/icons-material/Menu';
+import {useMediaQuery} from "@mui/material";
+
 
 
 
 export default function MainHeader(){
+
+  const responsivo = useMediaQuery(theme.breakpoints.up('sm'))
 
   return (
     <ThemeProvider theme={theme}>
@@ -20,7 +25,7 @@ export default function MainHeader(){
           justifyContent:'space-between'
           }}>
 
-        <Box 
+        <Box       
           sx={{
             display: 'flex',
             gap: '100px',
@@ -28,16 +33,23 @@ export default function MainHeader(){
             height: '41px',
             alignItems:'center'              
           }}>
-          <Box>
-            <img src={logoOrange} alt="Logo Orange Juice" width='111px' height='41px'/>  
-          </Box>
+          <Box sx={{display:'flex', gap:'16px', alignItems:'center'}}>
+            {!responsivo ?
+            <Box color='neutral.lightest'>
+              <MenuIcon/>
+            </Box>
+             : null}
+            <Box>
+              <img src={logoOrange} alt="Logo Orange Juice" width='111px' height='41px'/>  
+            </Box>
+           </Box>
 
-          <Box 
+         { responsivo ? <Box 
             sx={{
               display:'flex',
               gap: '24px',
               alignItems:'center',
-              width:'242px'
+              width:'242px',
             }}
               >
             <Typography 
@@ -48,7 +60,7 @@ export default function MainHeader(){
                 
                 }}
             >
-              Meus Projetos
+              Meus projetos
             </Typography>
             <Typography 
               color='neutral.lightest'
@@ -61,7 +73,7 @@ export default function MainHeader(){
             >
               Descobrir
             </Typography>
-          </Box>       
+          </Box> : null}      
         </Box>
         <Box
           sx={{display:'flex', alignItems:'center', gap:'16px'}}>
