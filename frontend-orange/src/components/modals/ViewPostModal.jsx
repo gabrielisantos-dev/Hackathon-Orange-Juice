34bb90f4571ce12b63a5141965b508906cc0eba6
gabrielicristina <gabrielicristina189@gmail.com>
2024-01-29 19:42:46 -0300

    style: posicionamentos e estilos concluídos

--- frontend-orange/src/components/modals/ViewPostModal.jsx
+++ frontend-orange/src/components/modals/ViewPostModal.jsx
@@ -1,50 +1,85 @@
-import { Modal, Box, Typography, ThemeProvider } from '@mui/material';
-import { theme } from '../../utils/Theme';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Chip from '@mui/material/Chip';
+import Link from '@mui/material/Link';
+import CloseIcon from '@mui/icons-material/Close';
+import picture from '../../assets/profile-picture/picture.svg';
+import projectImage from '../../assets/projects/project1.svg';
 
-const ViewPostModal = () => {
+const ViewPostModal = ({ onClose }) => {
   return (
-    <ThemeProvider theme={theme}>
-    <Modal open={true}>
-    <Box>
-      <Box
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          position: 'absolute',
-          width: '1600px',
-          height: '750px',
-          marginTop: '50px',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)',
-          backgroundColor: 'white',
-        }}
-      >
+    <Modal
+      open={true}
+      onClose={onClose}
+      aria-labelledby='modal-modal-title'
+      aria-describedby='modal-modal-description'
+      sx={{ overflow: 'scroll' }}
+    >
+      <div>
         <Box
           sx={{
-            width: '838px',
-            height: '50px',
-            top: '56px',
-            left: '102px',
-            gap: '175px',
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'row',
-            justifyContent: 'center',
+            position: 'absolute',
+            top: '75%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: '1042px',
+            height: '981px',
+            bgcolor: 'background.paper',
+            boxShadow: 24,
+            p: 3,
           }}
         >
-          <Box></Box>
-          <Typography variant="h4" font="Roboto">
-            Ecommerce One Page
-          </Typography>
-          <Box></Box>
+          <Box sx={{ display: 'flex', justifyContent: 'right' }}>
+            <CloseIcon onClick={onClose} style={{ cursor: 'pointer' }} />
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              margin: '0px 25px',
+            }}
+          >
+            <Box sx={{ display: 'flex', gap: '12px', alignItems: 'center' }}>
+              <img src={picture} alt='Imagem usuário' width='40px' height='40px' />
+              <Box>
+                <Typography variant='subtitle1'>Camila Soares</Typography>
+                <Typography variant='subtitle1'>12/12</Typography>
+              </Box>
+            </Box>
+            <Typography variant='h5'>Alguma coisa aqui</Typography>
+            <Box sx={{ display: 'flex', gap: '8px' }}>
+                <Chip />
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+            <img
+              src={projectImage}
+              alt='Imagem do projeto'
+              width='100%'
+              height='100%'
+              style={{ objectFit: 'cover' }}
+            />
+          </Box>
+          <Box
+            sx={{ height: '150px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', margin: '30px 25px' }}
+          >
+            <Typography variant='body1'>descrição</Typography>
+            <Box>
+              <Typography variant='subtitle1'>Download</Typography>
+              <Link href=''>Link</Link>
+            </Box>
+          </Box>
         </Box>
-      </Box>
-      </Box>
+      </div>
     </Modal>
-    </ThemeProvider>
   );
+};
+
+ViewPostModal.propTypes = {
+  onClose: PropTypes.func.isRequired,
 };
 
 export default ViewPostModal;
