ba43213c99e91ecbe06989354ece377331d57217
gabrielicristina <gabrielicristina189@gmail.com>
2024-01-30 00:05:29 -0300

    style: posicionamentos e estilo do modal concluído

--- frontend-orange/src/App.jsx
+++ frontend-orange/src/App.jsx
@@ -6,7 +6,8 @@
 // import BotaoGoogleLogin from "./components/tela-login/BotaoGoogleLogin"
 // import Header from "./components/meu-portifólio/Header"
 // import TelaLogin from "./pages/TelaLogin"
-import AddProjectModal from "./components/modals/AddProjectModal"
+// import AddProjectModal from "./components/modals/AddProjectModal"
+import SavePostModal from "./components/modals/SavePostModal"
 
 function App() {
 
@@ -20,7 +21,8 @@ function App() {
       {/* <TelaLogin/> */}
       {/* <Header/> */}
       {/* <ContainerImagemTelaLogin/> */}
-      <AddProjectModal />
+      {/* <AddProjectModal /> */}
+      <SavePostModal />
 
     </>
   )
