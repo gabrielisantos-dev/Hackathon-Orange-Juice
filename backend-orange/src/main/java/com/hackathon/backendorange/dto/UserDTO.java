8052686b1a9c5a6d3d706c406720e52390887d2d
Matheus rabelo <matheusrabelo520@gmail.com>
2024-02-01 19:58:35 -0300

    chore: Validação de entrada de dados do usuário

--- backend-orange/src/main/java/com/hackathon/backendorange/dto/UserDTO.java
+++ backend-orange/src/main/java/com/hackathon/backendorange/dto/UserDTO.java
@@ -1,6 +1,10 @@
 package com.hackathon.backendorange.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,8 +18,25 @@ import java.util.List;
 @NoArgsConstructor
 public class UserDTO  {
 	private Long id;
+
+	@NotBlank(message = "O campo 'nome' não pode estar em branco.")
+	@Pattern(regexp = "[a-zA-Z]+", message = "O campo 'nome' deve conter apenas letras (sem espaços).")
+	@Size(min = 3, max = 20, message = "O campo 'nome' deve ter entre 3 e 50 caracteres.")
 	private String nome;
+
+	@NotBlank(message = "O campo 'sobrenome' não pode estar em branco.")
+	@Pattern(regexp = "[a-zA-Z]+", message = "O campo 'sobrenome' deve conter apenas letras (sem espaços).")
+	@Size(min = 3, max = 50, message = "O campo 'sobrenome' deve ter entre 3 e 50 caracteres.")
 	private String sobrenome;
+
+	@NotBlank(message = "O campo 'email' não pode estar em branco.")
+	@Size(min = 5, max = 40, message = "O campo 'email' deve ter entre 5 e 100 caracteres.")
+	@Email(message = "O email precisa ser válido")
 	private String email;
+
+	@NotBlank(message = "O campo 'password' não pode estar em branco.")
+	@Size(min = 8, max = 20, message = "O campo 'password' deve ter entre 8 e 20 caracteres.")
+	@Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).*$", message = "A senha deve conter pelo menos uma letra maiúscula, um número e um caractere especial.")
 	private String password;
+
 }
