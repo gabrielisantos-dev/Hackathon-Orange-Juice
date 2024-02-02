4b39508f9dc1410c62f69e6a7feed4ff92e7a0e0
gabrielicristina <gabrielicristina189@gmail.com>
2024-02-01 23:14:54 -0300

    feat: iniciando funcionamento do botão google

--- frontend-orange/src/components/tela-login/BotaoGoogleLogin.jsx
+++ frontend-orange/src/components/tela-login/BotaoGoogleLogin.jsx
@@ -1,15 +1,40 @@
+import { useState } from 'react'
 import { Box } from '@mui/material'
-import botaoGoogle from '../../assets/tela-login/entrarComGoogle.svg'
-import Link from '@mui/material/Link';
-export default function BotaoGoogleLogin(props){
+import GoogleLogin from 'react-google-login';
+
+
+export default function BotaoGoogleLogin(){
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [profilePic, setProfilePic] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const responseGoogle = (response) => {
+    console.log(response);
+    const { profileObj: { name, email, imageUrl } } = response;
+    setName(name);
+    setEmail(email);
+    setProfilePic(imageUrl);
+    setIsLoggedIn(true);
+  }
 
   return (
 
     <Box>
-      <Link href='#'><img src={botaoGoogle} alt="Botao entrar com Google" width={props.width} height={props.height}/></Link>
+      <GoogleLogin
+      clientId='755287664476-qvq0arrdis429fdk0e2b7366cc60rtmo.apps.googleusercontent.com'
+      buttonText='Continuar com o Google'
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+        />
+        {isLoggedIn ? <div>
+        <h1>Informações do usuário</h1>
+        <img src={profilePic} alt='Profile' />
+        <p> Name: {name} </p>
+        <p> Email: {email} </p>
+       </div>
+        : null}
     </Box>
     
   )
 }
-
-
