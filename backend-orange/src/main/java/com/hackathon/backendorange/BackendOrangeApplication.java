274aa4f222ffe56f002d176bff8a93aec9c88389
Matheus rabelo <matheusrabelo520@gmail.com>
2024-02-02 15:43:22 -0300

    chore: Criando a liberação do cors para a aplicação

--- backend-orange/src/main/java/com/hackathon/backendorange/BackendOrangeApplication.java
+++ backend-orange/src/main/java/com/hackathon/backendorange/BackendOrangeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 public class BackendOrangeApplication {
