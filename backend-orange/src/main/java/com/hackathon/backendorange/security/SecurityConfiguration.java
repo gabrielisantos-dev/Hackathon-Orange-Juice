386cf26492c2a93fbf0b16af65c17329c4402924
Aira Arima <arimacoelhoaira@gmail.com>
2024-02-04 13:20:56 -0300

    refactor: adição de import que estava faltando

--- backend-orange/src/main/java/com/hackathon/backendorange/security/SecurityConfiguration.java
+++ backend-orange/src/main/java/com/hackathon/backendorange/security/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 package com.hackathon.backendorange.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
